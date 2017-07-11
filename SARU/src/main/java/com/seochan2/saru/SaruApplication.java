727524698fb38c9259b6077aebd01dcc0a5f3f86
seochan2 <seochan2@gmail.com>
2017-07-11 23:23:48 +0900

    - first commit

---
+++ SARU/src/main/java/com/seochan2/saru/SaruApplication.java
@@ -0,0 +1,12 @@
+package com.seochan2.saru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaruApplication.class, args);
+	}
+}
---
+++ SARU/src/test/java/com/seochan2/saru/SaruApplicationTests.java
@@ -0,0 +1,16 @@
+package com.seochan2.saru;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaruApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
