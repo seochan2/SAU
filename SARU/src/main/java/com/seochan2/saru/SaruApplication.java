6234813b260d9294a294adb7368fd7e2ce4a90eb
seochan2 <seochan2@gmail.com>
2017-07-13 22:31:47 +0900

    - CommandLineRunner 추가

--- SARU/src/main/java/com/seochan2/saru/SaruApplication.java
+++ SARU/src/main/java/com/seochan2/saru/SaruApplication.java
@@ -1,12 +1,19 @@
 package com.seochan2.saru;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SaruApplication {
+public class SaruApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaruApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("test");
+	}
+
 }
